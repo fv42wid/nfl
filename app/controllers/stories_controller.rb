4c6da0e3f125393f4054d3bae07a456ac159106e
Frank Vinci <fv42wid@yahoo.com>
2016-08-20 20:56:10 +0000

    did verification for create story, now create story

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,5 +1,6 @@
 class StoriesController < ApplicationController
   before_action :set_story, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_author!, only: [:new, :create]
 
   # GET /stories
   # GET /stories.json
